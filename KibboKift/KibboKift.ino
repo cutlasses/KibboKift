fa42914758bc6b21834fe03799f38626609b18ee
Cutlasses <spitkethly@gmail.com>
2022-07-17 17:03:44 +0100

    Technically working but not changing brightness

--- KibboKift/KibboKift.ino
+++ KibboKift/KibboKift.ino
@@ -28,36 +28,29 @@ void all_leds_off()
 
 void fade(float fade_time, bool fade_in)
 {
-  constexpr int min_freq          = 200;
-  constexpr float min_time        = 1.0f / min_freq;
   constexpr int to_us             = 1000 * 1000;
-  constexpr int min_time_us       = min_time * to_us;
-  constexpr int half_min_time_us  = min_time_us / 2;
-  constexpr int max_freq          = 1000;
-  constexpr float max_time        = 1.0f / max_freq;
-  constexpr int max_time_us       = max_time * to_us;
-  constexpr int half_max_time_us  = max_time_us / 2;
-  constexpr int delta_time_us     = min_time_us - max_time_us;  // min time is higher because it's a lower frequency
+  constexpr int low_freq          = 10;
+  constexpr float low_time        = 1.0f / low_freq;
+  constexpr int low_time_us       = low_time * to_us;
+  constexpr int half_low_time_us  = low_time_us / 2;
+  constexpr int high_freq         = 10000;
+  constexpr float high_time       = 1.0f / high_freq;
+  constexpr int high_time_us      = high_time * to_us;
+  constexpr int half_high_time_us = high_time_us / 2;
+  constexpr int delta_time_us     = low_time_us - high_time_us;  // min time is higher because it's a lower frequency
   constexpr int update_freq       = 10;
 
   const int num_updates           = fade_time * update_freq;
-  const int update_time_us        = (fade_time / num_updates) * to_us;
+  const int half_update_time_us   = ((fade_time / num_updates) * to_us) / 2;
   const int cycle_inc             = (delta_time_us / num_updates); // in us
   const int half_cycle_inc        = fade_in ? -(cycle_inc / 2) : (cycle_inc / 2);
 
-  Serial.print("num_updates ");
-  Serial.print(num_updates);
-  Serial.print(" half_cycle_inc ");
-  Serial.println(cycle_inc);
-
-  int half_cycle_time_us        = fade_in ? half_min_time_us : half_max_time_us;
+  int half_cycle_time_us         = fade_in ? half_low_time_us : half_high_time_us;
   for( int u = 0; u < num_updates; ++u )
   {
-    const int cycles_per_update = update_time_us / (half_cycle_time_us);
-    Serial.print(half_cycle_time_us);
-    Serial.print(" ");
-    Serial.println(cycles_per_update);
-    for( int u = 0; u < cycles_per_update; ++u )
+    const int cycles_per_update = (half_update_time_us * 1000) / (half_cycle_time_us * 1000); // * 1000 to save float calc
+
+    for( int c = 0; c < cycles_per_update; ++c )
     {
       all_leds_on();
       delayMicroseconds(half_cycle_time_us);
@@ -67,15 +60,13 @@ void fade(float fade_time, bool fade_in)
 
     half_cycle_time_us          += half_cycle_inc;
   }
-
-  Serial.println("end");
 }
 
 void loop()
 {
   fade(5, true);
-  //fade(1, false);
+  fade(5, false);
   //digitalWrite(1,HIGH);
 
-  delay(10000);
+  delay(1000);
 }
